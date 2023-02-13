48ee6efd84741715d1c4e9cb657074ce04b1db58
Victor Godoy Batin da Silva <victorgodoybatindasilva@MacBook-Pro-de-Victor.local>
2023-02-13 20:30:16 -0300

    Scroll usando ScrollReveal

---
+++ script.js
@@ -0,0 +1,30 @@
+ScrollReveal({
+  distance: "600px",
+  duration: 2500,
+  delay: 400,
+});
+
+ScrollReveal().reveal(".main-title, .subtitle", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".sec-01 .image, .info", {
+  delay: 600,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".text-box", { delay: 700, origin: "right" });
+ScrollReveal().reveal(".media-icons i", {
+  delay: 500,
+  origin: "bottom",
+  interval: 200,
+});
+ScrollReveal().reveal(".sec-02 .image, .sec-03 .image", {
+  delay: 500,
+  origin: "top",
+});
+ScrollReveal().reveal(".media-info li", {
+  delay: 500,
+  origin: "left",
+  interval: 200,
+});
+
+window.scrollTo({
+  behavior: "smooth",
+});
